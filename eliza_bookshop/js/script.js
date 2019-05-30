3d1943507ab87cd716a481a3f130e56fd1d03313
Ira Ramos <elizairaramos07@gmail.com>
2019-05-30 19:54:42 +0800

    bookworm onlineshop design page

---
+++ eliza_bookshop/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+});
+});
+
+
+ //     $(document).ready(function() {
+ //    $("[rel='tooltip']").tooltip();    
+ 
+ //    $('.thumbnail').hover(
+ //        function(){
+ //            $(this).find('.caption').fadeIn(500); //.fadeIn(250)
+ //        },
+ //        function(){
+ //            $(this).find('.caption').fadeOut(500); //.fadeOut(205)
+ //        }
+ //    ); 
+ // });
